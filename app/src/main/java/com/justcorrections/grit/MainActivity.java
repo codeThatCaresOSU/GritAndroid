e86d5eb1e7c90096f0cfff60cc8fe61e1ff3268a
Gus Workman <gus.a.workman@gmail.com>
2017-09-25 21:00:36 -0400

    Added BottomNavigationView and outlined fragments

--- app/src/main/java/com/justcorrections/grit/MainActivity.java
+++ app/src/main/java/com/justcorrections/grit/MainActivity.java
@@ -1,13 +1,63 @@
 package com.justcorrections.grit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.justcorrections.grit.account.AccountFragment;
+import com.justcorrections.grit.map.MapFragment;
+import com.justcorrections.grit.mystery.MysteryFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+    private List<Fragment> fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = (BottomNavigationView) findViewById(R.id.main_bottom_nav);
+
+        // TODO: change these parameters as needed
+        fragments.add(MapFragment.newInstance("Parameter 1", "Parameter 2"));
+        fragments.add(AccountFragment.newInstance("Parameter 1", "Parameter 2"));
+        fragments.add(MysteryFragment.newInstance("Parameter 1", "Parameter 2"));
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_map:
+                        setSelected(0);
+                        break;
+                    case R.id.menu_account:
+                        setSelected(1);
+                        break;
+                    case R.id.menu_mystery:
+                        setSelected(2);
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        setSelected(0);
+    }
+
+    private void setSelected(int position) {
+        // TODO: add a nice transition using anim resource files
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_holder, fragments.get(position))
+                .commit();
     }
 }
