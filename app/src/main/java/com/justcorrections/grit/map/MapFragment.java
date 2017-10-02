f8763e6596ece5ec70365734a86cc3e5f000c760
Ian Willis <ianwillis98@gmail.com>
2017-10-02 18:56:49 -0400

    Testing out different button styles

--- app/src/main/java/com/justcorrections/grit/map/MapFragment.java
+++ app/src/main/java/com/justcorrections/grit/map/MapFragment.java
@@ -1,11 +1,17 @@
 package com.justcorrections.grit.map;
 
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.justcorrections.grit.R;
 
 /**
@@ -27,6 +33,19 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        Fragment fragment = getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment supportmapfragment = (SupportMapFragment) fragment;
+        supportmapfragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                if (ActivityCompat.checkSelfPermission(getContext(), permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getContext(), permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+            }
+        });
 
         return view;
     }
