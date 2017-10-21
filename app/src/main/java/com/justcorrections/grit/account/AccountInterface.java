79be72b1e4a723d82ed5791e7c7023ce316a28f3
Gus Workman <gus.a.workman@gmail.com>
2017-10-20 21:37:44 -0400

    Added a DatabaseHelper, to facilitate getting and setting different types of data. Also started the interface after logging in

--- app/src/main/java/com/justcorrections/grit/account/AccountInterface.java
+++ app/src/main/java/com/justcorrections/grit/account/AccountInterface.java
@@ -8,7 +8,8 @@ public interface AccountInterface {
     int TYPE_LOGIN = 0;
     int TYPE_RESET = 1;
     int TYPE_CREATE = 2;
-    int TYPE_ACCOUNT = 3;
+    int TYPE_DETAILS = 3;
+    int TYPE_ACCOUNT = 4;
 
     int getType();
 }
--- app/src/main/java/com/justcorrections/grit/utils/DatabaseHelper.java
+++ app/src/main/java/com/justcorrections/grit/utils/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.justcorrections.grit.utils;
 
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Gus on 10/2/2017.
@@ -10,22 +12,57 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class DatabaseHelper {
 
-    private FirebaseDatabase database;
-    private DatabaseReference users;
+    private static final String USERS = "Users";
+    private static final String OHIO_LOCATIONS = "OhioData";
+    private static final String BUSINESSES = "MapBusiness";
+    private static final String FIRST_NAME = "FirstName";
+    private static final String Last_Name = "LastName";
+    private static final String AGE = "Age";
+    // private static final String INTERESTS = "Interests";
 
-    // variables
-    public DatabaseHelper() {
+    private GritUser gritUser;
+
+    private static FirebaseDatabase database;
+    private static DatabaseReference users;
+    private static DatabaseReference locations;
+    private static DatabaseHelper helper;
+
+    private DatabaseHelper() {
         database = FirebaseDatabase.getInstance();
-        users = database.getReference("Users");
+        users = database.getReference(USERS);
     }
 
+    public static synchronized DatabaseHelper getInstance() {
+        if (helper == null) {
+            helper = new DatabaseHelper();
+        }
 
-    public void initializeUser(FirebaseUser gritUser) {
-
+        return helper;
     }
 
     public void setUserData(GritUser gritUser) {
+        DatabaseReference user = users.child(gritUser.getId());
+        user.child(FIRST_NAME).setValue(gritUser.getFirstName());
+        user.child(Last_Name).setValue(gritUser.getLastName());
+        user.child(AGE).setValue(gritUser.getAge());
+    }
 
+    // TODO: possibly get the data and return it to the calling activity with an interface
+    public void getUserData(GritUser gritUser, ValueEventListener callback) {
+        DatabaseReference user = users.child(gritUser.getId());
+        user.addListenerForSingleValueEvent(callback);
+    }
+
+    // basically allows for self implementation. Just note that getting the data is asyncronous, so
+    // it won't be immediately available. At the end of the onDataChange method, put a call to the
+    // method that you want to execute after the data is available.
+    public void getOhioLocations(ValueEventListener callback) {
+        locations.addListenerForSingleValueEvent(callback);
+    }
+
+    // adds the location to a randomly named node under the OhioData node
+    public void setLocation(MapLocation location) {
+        locations.push().setValue(location);
     }
 
 }
