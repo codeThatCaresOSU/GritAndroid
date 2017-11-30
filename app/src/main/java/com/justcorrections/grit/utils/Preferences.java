d214857950c3c311fddf20bd8dac96253ba1bfe0
gworkman <gus.a.workman@gmail.com>
2017-11-30 18:34:05 -0500

    Add preferences helper class

---
+++ app/src/main/java/com/justcorrections/grit/utils/Preferences.java
@@ -0,0 +1,12 @@
+package com.justcorrections.grit.utils;
+
+/**
+ * Created by Gus on 11/30/2017. Used as a manager for all of the SharedPreferences keys
+ */
+
+public class Preferences {
+
+    // add all preferences keys here
+
+    private static final String KEY_PROFILE_COMPLETED = "profile_completed";
+}
